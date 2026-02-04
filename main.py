7301cc8a9156342e50dc6442b536797f89ed4fba
johnatan121 <johnatan121@live.com>
2026-02-03 21:17:21 -0300

    Add power consumption and cost calculation

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+potencia = float(input('digite a potencia do eletro (em watts); '))
+horas = int(input('digite o numero de horas de uso por dias: ' ))
+dias = int(input('digite o numero de dias de uso: '))
+tarifa = float(input('digite o valor da tarifa de energia (R$/kwh) '))
 
+
+consumo = (potencia*horas*dias)/1000
+custo = consumo*tarifa
+
+print(f'consumo total: {consumo:.2f} kwh')
+print(f'custo total:{custo:.2f}')
